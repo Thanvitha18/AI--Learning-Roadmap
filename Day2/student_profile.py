f2cf80e85340569a32b84459b1b33265303f0aa5
Thanvitha18 <thanvithareddy18@gmail.com>
2026-06-04 18:30:53 +0530

    Day 2 completed - Python basics and mini projects

---
+++ Day2/student_profile.py
@@ -0,0 +1,8 @@
+name=input("Enter name:")
+age=int(input("enter age:"))
+college=input("enter college name:")
+fav_tec=input("enter fav technology:")
+print("Name:",name)
+print("Age:",age)
+print("College:",college)
+print("Favorite Technology:",fav_tec)
