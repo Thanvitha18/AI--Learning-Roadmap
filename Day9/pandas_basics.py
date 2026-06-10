92b94be7989afd538b4a9d80643f80ba4b199fea
Thanvitha18 <thanvithareddy18@gmail.com>
2026-06-10 23:19:05 +0530

    Day 9 pandas basics done

---
+++ Day9/pandas_basics.py
@@ -0,0 +1,19 @@
+import pandas as pd
+data={
+    "name":["ram","sita","krishna","radha","shiva"],
+    "Branch":["CSE","ECE","CSE","CAI","MEC"],
+    "Marks":[95,80,75,94,82]
+}
+df=pd.DataFrame(data)
+print(df)
+print(df.head(3))
+print(df.tail(2))
+print(df.info())
+print(df.describe())
+print(df["Marks"])
+print(df.loc[2])
+
+
+import pandas as pd
+df=pd.read_csv("students.csv")
+print(df)
