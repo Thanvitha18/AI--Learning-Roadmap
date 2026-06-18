f2797cb65cd909069dfba4e41f9092dda3e4989a
Thanvitha18 <thanvithareddy18@gmail.com>
2026-06-18 13:04:49 +0530

    Day18 Statistics

---
+++ Day18/statistics_basics.py
@@ -0,0 +1,8 @@
+import numpy as np
+import statistics
+data=[1,2,2,4,6,7,8,9,10]
+print("Mean: ",np.mean(data))
+print("Mode:", statistics.mode(data))
+print("Median: ",np.median(data))
+print("Variance: ",np.var(data))
+print("Standard Deviar=tion: ",np.std(data))
